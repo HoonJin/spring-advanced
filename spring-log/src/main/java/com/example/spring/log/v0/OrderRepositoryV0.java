d0926befd1bc98d53baf0413f2ec7e9945be775e
Daniel Ji <bwjhj1030@gmail.com>
2022-03-27 16:18:16 +0900

    김영한 스프링부트 고급편 강좌 공부용 프로젝트 생성

---
+++ spring-log/src/main/java/com/example/spring/log/v0/OrderRepositoryV0.java
@@ -0,0 +1,25 @@
+package com.example.spring.log.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("exception");
+        }
+
+        sleep(1000);
+    }
+
+    private void sleep(int i) {
+        try {
+            Thread.sleep(i);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
