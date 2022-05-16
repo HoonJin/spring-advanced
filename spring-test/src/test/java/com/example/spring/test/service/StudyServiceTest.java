8b14d6ee10733dbd7da841f0efb82032ad58d69c
Daniel Ji <bwjhj1030@gmail.com>
2022-05-16 18:00:33 +0900

    BDD style, given, when, then 방식을 더 적절해보이는 방식으로 테스트

--- spring-test/src/test/java/com/example/spring/test/service/StudyServiceTest.java
+++ spring-test/src/test/java/com/example/spring/test/service/StudyServiceTest.java
@@ -9,8 +9,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
@@ -23,22 +24,30 @@ class StudyServiceTest {
 
     @Test
     void createStudyTest() {
+        // GIVEN
 //        MemberService memberService = mock(MemberService.class);
 //        StudyRepository studyRepository = mock(StudyRepository.class);
 //        System.out.println("memberService = " + memberService);
 //        System.out.println("studyRepository = " + studyRepository);
+        StudyService studyService = new StudyService(memberService, studyRepository);
+        assertNotNull(studyService);
 
         Member newMember = new Member();
         newMember.setId(1L);
         newMember.setEmail("a@a.com");
-        when(memberService.findById(1L)).thenReturn(newMember);
-
-        StudyService studyService = new StudyService(memberService, studyRepository);
-        assertNotNull(studyService);
+        // when(memberService.findById(1L)).thenReturn(newMember);
+        given(memberService.findById(1L)).willReturn(newMember);
 
         Study study = new Study(10);
 
+        // WHEN
         studyService.createStudy(1L, study);
+
+        // THEN
+        // verify 는 몇번 호출되었는가 검증함.
+        //verify(studyRepository, times(1)).save(study);
+        then(studyRepository).should(times(1)).save(study);
+        then(studyRepository).shouldHaveNoMoreInteractions();
 
         assertEquals("a@a.com", study.getOwner().getEmail());
     }
