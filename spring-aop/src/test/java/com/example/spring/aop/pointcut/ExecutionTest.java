cb4e2e6db6a65bbd10f2c1866f5cb5dfdcecfb3f
Daniel Ji <bwjhj1030@gmail.com>
2022-05-22 21:37:42 +0900

    execution 식에 대한 예제 추가

--- spring-aop/src/test/java/com/example/spring/aop/pointcut/ExecutionTest.java
+++ spring-aop/src/test/java/com/example/spring/aop/pointcut/ExecutionTest.java
@@ -94,4 +94,61 @@ public class ExecutionTest {
         assertThat(pointcut.matches(method, MemberServiceImpl.class)).isTrue();
     }
 
+    @Test
+    void typeExactMatch() {
+        pointcut.setExpression("execution(* com.example.spring.aop.member.MemberServiceImpl.*(..))");
+        assertThat(pointcut.matches(method, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void typeMatchSuperType() {
+        pointcut.setExpression("execution(* com.example.spring.aop.member.MemberService.*(..))");
+        assertThat(pointcut.matches(method, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void typeMatchInternal() throws NoSuchMethodException {
+        pointcut.setExpression("execution(* com.example.spring.aop.member.MemberServiceImpl.*(..))");
+        Method internalMethod = MemberServiceImpl.class.getMethod("internal", String.class);
+        assertThat(pointcut.matches(internalMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void typeMatchNoSuperTypeMethodFalse() throws NoSuchMethodException {
+        pointcut.setExpression("execution(* com.example.spring.aop.member.MemberService.*(..))");
+        Method internalMethod = MemberServiceImpl.class.getMethod("internal", String.class);
+        // 당연하지만 상위 타입에 있는 메서드만 사용 가능
+        assertThat(pointcut.matches(internalMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @Test
+    void argsMatch() {
+        pointcut.setExpression("execution(* com.example.spring.aop.member.MemberService.*(String))");
+        assertThat(pointcut.matches(method, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void noArgsMatch() {
+        pointcut.setExpression("execution(* com.example.spring.aop.member.MemberService.*())");
+        assertThat(pointcut.matches(method, MemberServiceImpl.class)).isFalse();
+    }
+
+    @Test
+    void oneArgsMatchStar() {
+        pointcut.setExpression("execution(* com.example.spring.aop.member.MemberService.*(*))");
+        assertThat(pointcut.matches(method, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void oneArgsMatchAll() {
+        pointcut.setExpression("execution(* com.example.spring.aop.member.MemberService.*(..))");
+        assertThat(pointcut.matches(method, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void oneArgsMatchWithStringAndAll() {
+        pointcut.setExpression("execution(* com.example.spring.aop.member.MemberService.*(String, ..))");
+        assertThat(pointcut.matches(method, MemberServiceImpl.class)).isTrue();
+    }
+
 }
