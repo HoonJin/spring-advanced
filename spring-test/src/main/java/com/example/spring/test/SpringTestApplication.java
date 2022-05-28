1d7bf2f948de1fd773a5c3af25d09201c11c9ae1
Daniel Ji <bwjhj1030@gmail.com>
2022-05-28 22:29:12 +0900

    스트레스 테스트를 위한 chaos monkey 라이브러리 적용

--- spring-test/src/main/java/com/example/spring/test/SpringTestApplication.java
+++ spring-test/src/main/java/com/example/spring/test/SpringTestApplication.java
@@ -1,4 +1,12 @@
 package com.example.spring.test;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestApplication.class);
+    }
 }
