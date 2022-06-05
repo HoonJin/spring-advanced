73e0ac5a5a1936249018842c4195948f3c7a9ef1
Daniel Ji <bwjhj1030@gmail.com>
2022-06-06 00:04:45 +0900

    html을 타임리프로 작성하는 예제 추가 & 셀렉트 박스 예제 추가

---
+++ spring-form/src/main/java/com/example/spring/form/item/ItemType.java
@@ -0,0 +1,14 @@
+package com.example.spring.form.item;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum ItemType {
+
+    BOOK("도서"), FOOD("음식"), ETC("기타");
+
+    private final String description;
+
+}
