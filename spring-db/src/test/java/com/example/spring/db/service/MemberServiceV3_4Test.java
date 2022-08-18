a11dd30005fc54c0278aa3e235735a649bc58a65
Daniel Ji <bwjhj1030@gmail.com>
2022-08-19 01:14:17 +0900

    autoconfigration 을 이용한 테스트 처리 도입

---
+++ spring-db/src/test/java/com/example/spring/db/service/MemberServiceV3_4Test.java
@@ -0,0 +1,91 @@
+
+package com.example.spring.db.service;
+
+import com.example.spring.db.domain.Member;
+import com.example.spring.db.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+import static com.example.spring.db.connection.ConnectionConst.*;
+
+@SpringBootTest // spring aop 를 적용하기 위해선 당연히 SpringBootTest 어노테이션을 사용해야 한다.
+@Slf4j
+class MemberServiceV3_4Test {
+
+    public static final String MemberA = "memberA";
+    public static final String MemberB = "memberB";
+    public static final String MemberEX = "ex";
+
+    @Autowired
+    private MemberRepositoryV3 memberRepository;
+
+    @Autowired
+    private MemberServiceV3_3 memberService;
+
+    @AfterEach
+    void after() {
+        // 좀 구리지만 하나씩 다 지워준다.
+        memberRepository.delete(MemberA);
+        memberRepository.delete(MemberB);
+        memberRepository.delete(MemberEX);
+    }
+
+    @Test
+    void accountTransfer() {
+        // given
+        Member memberA = new Member(MemberA, 10000);
+        Member memberB = new Member(MemberB, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        // when
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+
+        // then
+        Member byIdA = memberRepository.findById(memberA.getMemberId());
+        Member byIdB = memberRepository.findById(memberB.getMemberId());
+
+        Assertions.assertThat(byIdA.getMoney()).isEqualTo(8000);
+        Assertions.assertThat(byIdB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    void aopCheck() {
+        log.info("memberRepository = {}", memberRepository.getClass());
+        log.info("memberService = {}", memberService.getClass());
+
+        Assertions.assertThat(AopUtils.isAopProxy(memberRepository)).isTrue();
+        Assertions.assertThat(AopUtils.isAopProxy(memberService)).isTrue();
+    }
+
+    @Test
+    void accountTransferEX() {
+        // given
+        Member memberA = new Member(MemberA, 10000);
+        Member memberEX = new Member(MemberEX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEX);
+
+        // when
+        Assertions.assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEX.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+        // then
+        Member byIdA = memberRepository.findById(memberA.getMemberId());
+        Member byIdB = memberRepository.findById(memberEX.getMemberId());
+
+        Assertions.assertThat(byIdA.getMoney()).isEqualTo(10000);
+        Assertions.assertThat(byIdB.getMoney()).isEqualTo(10000);
+    }
+}
