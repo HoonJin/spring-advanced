7b43209ec007de2c490e01e337889a20e418653a
Daniel Ji <bwjhj1030@gmail.com>
2022-08-24 00:15:54 +0900

    스프링 익셉션 서블릿 내 처리 프로젝트 생성 및 커스텀 에러페이지 만듬

---
+++ spring-exception/src/main/java/com/example/spring/exception/servlet/ErrorPageController.java
@@ -0,0 +1,25 @@
+package com.example.spring.exception.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@Slf4j
+public class ErrorPageController {
+
+    @RequestMapping("/error-page/404")
+    public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
+        log.error("not found error page");
+        return "error-page/404";
+    }
+
+    @RequestMapping("/error-page/500")
+    public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
+        log.error("internal server error page");
+        return "error-page/500";
+    }
+}
