e6a8532497906515f7d1bf753d6659f53bcc1202
Daniel Ji <bwjhj1030@gmail.com>
2022-08-27 00:36:47 +0900

    에러 페이지로 넘어갈 때 필터와 인터셉터를 모두 거쳐야하는데 이를 필터링 또는 거치도록 제어할수 있도록 필터와 인터셉터 구현

--- spring-exception/src/main/java/com/example/spring/exception/servlet/ErrorPageController.java
+++ spring-exception/src/main/java/com/example/spring/exception/servlet/ErrorPageController.java
@@ -11,15 +11,38 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class ErrorPageController {
 
+    // from RequestDispatcher
+    public static final String ERROR_EXCEPTION = "javax.servlet.error.exception";
+    public static final String ERROR_EXCEPTION_TYPE = "javax.servlet.error.exception_type";
+    public static final String ERROR_MESSAGE = "javax.servlet.error.message";
+    public static final String ERROR_REQUEST_URI = "javax.servlet.error.request_uri";
+    public static final String ERROR_SERVLET_NAME = "javax.servlet.error.servlet_name";
+    public static final String ERROR_STATUS_CODE = "javax.servlet.error.status_code";
+
+
     @RequestMapping("/error-page/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
-        log.error("not found error page");
+        printErrorInfo(request);
         return "error-page/404";
     }
 
     @RequestMapping("/error-page/500")
     public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
-        log.error("internal server error page");
+        printErrorInfo(request);
         return "error-page/500";
+    }
+
+    private void printErrorInfo(HttpServletRequest request) {
+//        request.getAttributeNames().asIterator()
+//                .forEachRemaining(name -> log.info("{} = {}", name, request.getAttribute(name)));
+
+        log.info("REQUEST ERROR_EXCEPTION {}", request.getAttribute(ERROR_EXCEPTION));
+        log.info("REQUEST ERROR_EXCEPTION_TYPE {}", request.getAttribute(ERROR_EXCEPTION_TYPE));
+        log.info("REQUEST ERROR_MESSAGE {}", request.getAttribute(ERROR_MESSAGE));
+        log.info("REQUEST ERROR_REQUEST_URI {}", request.getAttribute(ERROR_REQUEST_URI));
+        log.info("REQUEST ERROR_SERVLET_NAME {}", request.getAttribute(ERROR_SERVLET_NAME));
+        log.info("REQUEST ERROR_STATUS_CODE {}", request.getAttribute(ERROR_STATUS_CODE));
+        log.error("DISPATCHER TYPE: {}", request.getDispatcherType());
+        log.info("");
     }
 }
