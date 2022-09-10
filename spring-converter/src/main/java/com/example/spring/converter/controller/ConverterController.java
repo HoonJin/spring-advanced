966f9e3b8a7b6981f43d3b1318ebf76aabde1e58
Daniel Ji <bwjhj1030@gmail.com>
2022-09-11 00:18:25 +0900

    converter controller와 view 로 thymeleaf 에서 어떻게 표현하는지 예제 추가

---
+++ spring-converter/src/main/java/com/example/spring/converter/controller/ConverterController.java
@@ -0,0 +1,37 @@
+package com.example.spring.converter.controller;
+
+import com.example.spring.converter.type.IpPort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ConverterController {
+
+    @GetMapping("/converter-view")
+    public String converterView(Model model) {
+        model.addAttribute("number", 10000);
+        model.addAttribute("ipPort", new IpPort("127.0.0.1", 8000));
+        return "converter-view";
+    }
+
+    @GetMapping("/converter/edit")
+    public String converterForm(Model model) {
+        IpPort ipPort = new IpPort("127.0.0.1", 8000);
+        Form form = new Form(ipPort);
+        model.addAttribute("form", form);
+        return "converter-form";
+    }
+
+    @PostMapping("/converter/edit")
+    public String converterEdit(@ModelAttribute Form form, Model model) {
+        IpPort ipPort = form.ipPort();
+
+        model.addAttribute("ipPort", ipPort);
+        return "converter-view";
+    }
+
+    record Form(IpPort ipPort) {}
+}
