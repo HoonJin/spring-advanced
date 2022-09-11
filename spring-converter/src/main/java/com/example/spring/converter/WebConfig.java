65e9e7ca86a9ae226f3301cce26d033554722c10
Daniel Ji <bwjhj1030@gmail.com>
2022-09-12 00:16:43 +0900

    더이상 큰의미를 느낄수 없을정도로 동네 수준 쇠락

--- spring-converter/src/main/java/com/example/spring/converter/WebConfig.java
+++ spring-converter/src/main/java/com/example/spring/converter/WebConfig.java
@@ -4,6 +4,7 @@ import com.example.spring.converter.converter.IntegerToStringConverter;
 import com.example.spring.converter.converter.IpPortToStringConverter;
 import com.example.spring.converter.converter.StringToIntegerConverter;
 import com.example.spring.converter.converter.StringToIpPortConverter;
+import com.example.spring.converter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,8 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        registry.addFormatter(new MyNumberFormatter());
+
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
     }
