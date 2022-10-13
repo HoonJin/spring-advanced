5443171695c4748f08f063170ae3711079bdff51
Daniel Ji <bwjhj1030@gmail.com>
2022-10-14 01:08:04 +0900

    트랜잭션이 실패할 경우에 대한 테스트 추가

--- spring-dbtx/src/test/java/com/example/spring/dbtx/propagation/MemberServiceTest.java
+++ spring-dbtx/src/test/java/com/example/spring/dbtx/propagation/MemberServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.spring.dbtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +18,34 @@ class MemberServiceTest {
 
     @Test
     void outerTxOff_success() {
+        // given
+        String username = "outerTxOff_success";
+
+        // when
+        memberService.joinV1(username);
+
+        // then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+    @Test
+    // MemberService 에 트랜잭션 annotation 코멘트 처리하면 성공
+    void outerTxOff_fail() {
+        // given
+        String username = "예외 outerTxOff_fail";
+
+        // when
+        Assertions.assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        // then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    @Test
+    void singleTx() {
         // given
         String username = "outerTxOff_success";
 
