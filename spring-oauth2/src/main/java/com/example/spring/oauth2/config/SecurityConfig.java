db168f2bd7d4244a1827c5590dbdc7603ff049f2
Daniel Ji <bwjhj1030@gmail.com>
2022-11-05 22:27:33 +0900

    authentication entry point 커스텀 구현 추가와 기본 구현에 대한 레퍼런스 추가

--- spring-oauth2/src/main/java/com/example/spring/oauth2/config/SecurityConfig.java
+++ spring-oauth2/src/main/java/com/example/spring/oauth2/config/SecurityConfig.java
@@ -4,7 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 //public class SecurityConfig extends WebSecurityConfigurerAdapter {
 @EnableWebSecurity
@@ -22,6 +29,14 @@ public class SecurityConfig {
         http.apply(customSecurityConfigurer);
         http.formLogin();
         http.httpBasic();
+        http.exceptionHandling().authenticationEntryPoint(
+                // AuthenticationEntryPoint 인터페이스 구현체 참조
+                // DelegatingAuthenticationEntryPoint 가 상황에 맞는 예외 entryPoint 를 찾아서 적절한 에러를 줌
+                (request, response, authException) -> {
+                    System.out.println("request = " + request);
+                    System.out.println("authException = " + authException);
+                    response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied");
+                });
 
         return http.build();
     }
