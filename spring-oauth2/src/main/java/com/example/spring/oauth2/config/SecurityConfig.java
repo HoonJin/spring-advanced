5765d10828ec007e0d5f46bc0751bb72b3af122e
Daniel Ji <bwjhj1030@gmail.com>
2022-11-06 19:53:43 +0900

    basic 인증 처리에 대하여 session 을 사용하지 않도록 커스터마이징한 예제 구현

--- spring-oauth2/src/main/java/com/example/spring/oauth2/config/SecurityConfig.java
+++ spring-oauth2/src/main/java/com/example/spring/oauth2/config/SecurityConfig.java
@@ -1,9 +1,10 @@
 package com.example.spring.oauth2.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,18 +28,32 @@ public class SecurityConfig {
         customSecurityConfigurer.setSecured(false);
 
         http.apply(customSecurityConfigurer);
-        http.formLogin();
-        http.httpBasic();
-        http.exceptionHandling().authenticationEntryPoint(
-                // AuthenticationEntryPoint 인터페이스 구현체 참조
-                // DelegatingAuthenticationEntryPoint 가 상황에 맞는 예외 entryPoint 를 찾아서 적절한 에러를 줌
-                (request, response, authException) -> {
-                    System.out.println("request = " + request);
-                    System.out.println("authException = " + authException);
-                    response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied");
-                });
+//        http.formLogin();
+        http.httpBasic()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+//        http.exceptionHandling().authenticationEntryPoint(
+//                // AuthenticationEntryPoint 인터페이스 구현체 참조
+//                // DelegatingAuthenticationEntryPoint 가 상황에 맞는 예외 entryPoint 를 찾아서 적절한 에러를 줌
+//                (request, response, authException) -> {
+//                    System.out.println("request = " + request);
+//                    System.out.println("authException = " + authException);
+//                    response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied");
+//                });
 
         return http.build();
+    }
+
+    static class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+        @Override
+        public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+            response.addHeader("WWW-Authenticate", "Basic realm=\"localhost\"");
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        }
     }
 
 //    @Override
